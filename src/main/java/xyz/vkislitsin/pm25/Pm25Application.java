d155100c675a9fdc6c94e2fe0b4d61d9fd988285
Vladimir Kislitsin <v.kislitsin@forward.lc>
2023-05-28 19:48:37 +0600

    initial commit

---
+++ src/main/java/xyz/vkislitsin/pm25/Pm25Application.java
@@ -0,0 +1,13 @@
+package xyz.vkislitsin.pm25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pm25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pm25Application.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/vkislitsin/pm25/Pm25ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.vkislitsin.pm25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pm25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
